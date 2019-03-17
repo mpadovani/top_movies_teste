3a2c7c7e0578bb3c9b99da85887730dcd7a4e8c8
Mateus Padovani <mateuspadovani@gmail.com>
2019-03-17 18:18:46 -0300

    Remove Pods

--- Pods/Target Support Files/Pods-top_movies_teste/Pods-top_movies_teste-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_top_movies_teste : NSObject
-@end
-@implementation PodsDummy_Pods_top_movies_teste
-@end
--- Pods/Target Support Files/Pods-top_movies_testeTests/Pods-top_movies_testeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_top_movies_testeTests : NSObject
-@end
-@implementation PodsDummy_Pods_top_movies_testeTests
-@end
